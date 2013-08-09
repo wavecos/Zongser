e49f7ac4ecf28bc53f1cdb4e67dbd27aa512c0e4
Alfredo Arias <alfre.arias@gmail.com>
2013-08-08 23:07:57 -0400

    tests in angularjs

--- www/controller_tests.js
+++ www/controller_tests.js
@@ -1,8 +1,22 @@
-function HelloController($scope) {
-	$scope.greeting = { text: "Hello" };
+
+var myAppModule = angular.module('myApp', []);
+
+myAppModule.controller('HelloController', function ($scope) {
+	var greeting = { text: "Hello" };
 
 	$scope.resetToDefault = function() {
-		$scope.greeting = { text: "eliminado" };
+		$scope.greeting = greeting;
+	};
+});
+
+myAppModule.controller('MenuController', function ($scope) {
+
+	$scope.menuState = { show: false };
+
+	$scope.toggleMenu = function () {
+		$scope.menuState.show = !$scope.menuState.show;
 	};
 
-}
+});
+
+
---
+++ www/js/Zongser.js
@@ -0,0 +1,1 @@
+var zongerAppModule = angular.module('ZongserApp', []);
