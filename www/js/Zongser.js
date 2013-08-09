e08444e86ade94a537775fda7c2cc30f85f0a1f1
Alfredo Arias <alfre.arias@gmail.com>
2013-08-09 18:18:26 -0400

    integrated a search filter to catch 10 songs in iTunes

--- www/js/Zongser.js
+++ www/js/Zongser.js
@@ -1,22 +1,42 @@
 var zongerAppModule = angular.module('ZongserApp', []);
 
-
+/*
 zongerAppModule.config(function($httpProvider){
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 });
+*/
 
 /* Controllers */
+zongerAppModule.controller('ListSongController', function($scope, $http) {
 
-zongerAppModule.controller('ListSongController', function($scope, $http){
-	$scope.hello = "Hello World!";
+	$scope.searchSongs = function() {
+		var searchCriteria = encodeURIComponent($scope.search);
+		// take a look in callback=JSON_CALLBACK in the url, I use this for JSONP callback
+		var iTunesUrl = 'https://itunes.apple.com/search?callback=JSON_CALLBACK&term=' + searchCriteria + '&entity=song&limit=10';
 
-	$http.get('dummy_songs.json').success(function(data, status, headers, config) {
-		console.log('data received :: ' + data);
-	});
+		console.log('search criteria ' + searchCriteria);
+		// Get top 10 Songs from iTunes url
+		$http.jsonp(iTunesUrl).success(function(data, status, headers, config) {
+			$scope.songs = data.results;
+		});
+
+	};
 
 });
 
 /* Routes */
 
-
+zongerAppModule.config(function($routeProvider) {
+	$routeProvider.
+		when('/', {
+			controller: 'ListSongController',
+			templateUrl: 'list.html'
+		}).
+		when('/view/:id', {
+			controller: 'DetailController',
+			templateUrl: 'detail.html'
+		}).
+		otherwise({
+			redirectTo: '/'});
+});
 
