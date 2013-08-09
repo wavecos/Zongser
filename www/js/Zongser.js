2120c3fc57ad7b4b03113977128df6ed5abb1b9b
Alfredo Arias <alfre.arias@gmail.com>
2013-08-09 12:20:01 -0400

    test in call json

--- www/js/Zongser.js
+++ www/js/Zongser.js
@@ -1,1 +1,22 @@
 var zongerAppModule = angular.module('ZongserApp', []);
+
+
+zongerAppModule.config(function($httpProvider){
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+/* Controllers */
+
+zongerAppModule.controller('ListSongController', function($scope, $http){
+	$scope.hello = "Hello World!";
+
+	$http.get('dummy_songs.json').success(function(data, status, headers, config) {
+		console.log('data received :: ' + data);
+	});
+
+});
+
+/* Routes */
+
+
+
